28413e8defa608ca02af961b2827a4be7771b119
NajlaaTar <NajlaaTar@users.noreply.github.com>
2017-08-09 07:11:04 +0300

    Add Note Project

---
+++ MakeSchoolNotes-Swift-V2-Starter-swift3-coredata/MakeSchoolNotes/Controllers/DisplayNoteViewController.swift
@@ -0,0 +1,47 @@
+//
+//  DisplayNoteViewController.swift
+//  MakeSchoolNotes
+//
+//  Created by Chris Orcutt on 1/10/16.
+//  Copyright © 2016 MakeSchool. All rights reserved.
+//
+
+import UIKit
+
+class DisplayNoteViewController: UIViewController {
+    var note: Note?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.darkGray
+    }
+    
+    @IBOutlet weak var noteTitleTextField: UITextField!
+    
+    @IBOutlet weak var noteContentTextView: UITextView!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if let note = note {
+            noteTitleTextField.text = note.title
+            noteContentTextView.text = note.content
+        } else {
+            noteTitleTextField.text = ""
+            noteContentTextView.text = ""
+        }
+    }
+        
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "save" {
+            // if note exists, update title and content
+            let note = self.note ?? CoreDataHelper.newNote()
+            note.title = noteTitleTextField.text ?? ""
+            note.content = noteContentTextView.text ?? ""
+            note.modificationTime = Date() as NSDate
+            CoreDataHelper.saveNote()
+        }
+    }
+    
+}
